428ee97ef6db7f7e0c992859dda81a2619af2219
Reggie Granados <rgranados@gmail.com>
2011-09-21 00:28:59 -0400

    DB

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,15 @@ set :default_environment, { 'PATH' => "'/usr/lib/ruby/gems/1.8/bin//bundle:/home
 
 ssh_options[:forward_agent] = true
 
+
+
+symlink_configuration = [
+  %w(config/database.yml    config/database.yml),
+  %w(db/production.sqlite3  db/production.sqlite3),
+  %w(system                 public/system)
+]
+
+
 # additional settings
 set :chmod755, "app config db lib public vendor script script/* public/disp*"
 set :use_sudo, false
@@ -39,6 +48,62 @@ end
 namespace :deploy do
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
+  end
+
+
+namespace :db do
+  
+    desc "Syncs the database.yml file from the local machine to the remote machine"
+    task :sync_yaml do
+      puts "\n\n=== Syncing database yaml to the production server! ===\n\n"
+      unless File.exist?("config/database.yml")
+        puts "There is no config/database.yml.\n "
+        exit
+      end
+      system "rsync -vr --exclude='.DS_Store' config/database.yml #{user}@#{application}:#{shared_path}/config/"
+    end
+    
+    desc "Create Production Database"
+    task :create do
+      puts "\n\n=== Creating the Production Database! ===\n\n"
+      run "cd #{current_path}; bundle exec rake db:create RAILS_ENV=production"
+      system "cap deploy:set_permissions"
+    end
+  
+    desc "Migrate Production Database"
+    task :migrate do
+      puts "\n\n=== Migrating the Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:migrate RAILS_ENV=production"
+      system "cap deploy:set_permissions"
+    end
+
+    desc "Resets the Production Database"
+    task :migrate_reset do
+      puts "\n\n=== Resetting the Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:migrate:reset RAILS_ENV=production"
+    end
+    
+    desc "Destroys Production Database"
+    task :drop do
+      puts "\n\n=== Destroying the Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:drop RAILS_ENV=production"
+      system "cap deploy:set_permissions"
+    end
+
+    desc "Moves the SQLite3 Production Database to the shared path"
+    task :move_to_shared do
+      puts "\n\n=== Moving the SQLite3 Production Database to the shared path! ===\n\n"
+      run "mv #{current_path}/db/production.sqlite3 #{shared_path}/db/production.sqlite3"
+      system "cap deploy:setup_symlinks"
+      system "cap deploy:set_permissions"
+    end
+  
+    desc "Populates the Production Database"
+    task :seed do
+      puts "\n\n=== Populating the Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:seed RAILS_ENV=production"
+    end
+  
   end
 end
 
